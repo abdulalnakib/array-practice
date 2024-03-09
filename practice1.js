a25e370ff6439af1d6ba13fa980d4601b78e774e
abdulalnakib <abdulalnakib10@gmail.com>
2024-03-09 20:06:12 +0600

    finished the first practice

--- practice1.js
+++ practice1.js
@@ -1,1 +1,8 @@
-var
+// Find index of Banana and replace it with Mango
+var fruits = ['Apple', 'Banana', 'Orange'];
+var findPosition = fruits.indexOf('Banana');
+fruits[1] = 'Mango';
+// Remove Orange and add Watermelon
+fruits.pop();
+fruits.push('Watermelon');
+console.log(fruits)
