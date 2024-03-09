c91e4c6e43001bab5cd4b95aa2a086d5a868b415
abdulalnakib <abdulalnakib10@gmail.com>
2024-03-09 21:52:24 +0600

    finihed the second practice

---
+++ practice2.js
@@ -0,0 +1,20 @@
+var tom = 66;
+var jane = 95;
+var peter = 56;
+var john = 40;
+var my = 85;
+if (john >= 80) {
+    console.log('A Grade')
+}
+else if (john >= 60) {
+    console.log('B Grade')
+}
+else if (john >= 50) {
+    console.log('C Grade')
+}
+else if (john >= 40) {
+    console.log('D Grade')
+}
+else if (john <= 39) {
+    console.log('F Grade')
+}
